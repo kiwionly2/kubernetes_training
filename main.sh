509b582337671e9a579b2ccbac75816443fef4db
stv707 <stv707@gmail.com>
2021-10-18 21:22:40 +0800

    v2

--- main.sh
+++ main.sh
@@ -23,9 +23,9 @@ if [ $# -ne 1 ]
   'vm001') createrg_vm001 && config_vm001 ;;
   'k8s'  ) non ;;#createrg_k8s  && config_k8s ;;
   'cloud') non ;;#createrg_cloud ;;
-  'aks'  ) createrg_cloud_aks && info_aks > $HOME/aks_info.txt ;;
+  'aks'  ) createrg_cloud_aks && info_aks > $HOME/aks_info.txt && createrg_vm001 && config_vm001 ;;
   'init' ) reinit   ;;
-  'info' ) info_aks ;; 
+  'info' ) info_aks ;;
         *) my_usage ;;
   esac
 fi
